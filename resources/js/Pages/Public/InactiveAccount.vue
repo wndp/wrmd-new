95a326b50a7a6e941ace5eb3426dfdfae434dbcd
Devin 2015 MacBook <dddesigns3@gmail.com>
2024-12-13 14:36:25 -0800

    Update Inactive Account Policy text

--- resources/js/Pages/Public/InactiveAccount.vue
+++ resources/js/Pages/Public/InactiveAccount.vue
@@ -7,9 +7,32 @@
         </h1>
       </div>
       <div class="mt-6 prose max-w-none prose-blue text-gray-500 mx-auto">
-        <h4>Last updated: Sep 10, 2023</h4>
-        <p>This Inactive Account Policy describes Our policies and procedures on ....</p>
-        ...
+        <h4>Last updated: March 15, 2025</h4>
+        <p>This Inactive Account Policy describes Our policies and procedures related to the management of Accounts that have been Inactive for a specified period of time.</p>
+        <h2>Interpretation and Definitions</h2>
+        <h3>Interpretation</h3>
+        <p>The words of which the initial letter is capitalized have meanings defined under the following conditions. The following definitions shall have the same meaning regardless of whether they appear in singular or in plural.</p>
+        <h3>Definitions</h3>
+        <p>For the purposes of this Privacy Policy:</p>
+        <ul>
+          <li>
+            <p><strong>Account</strong> means a unique account created for You to access our Service or parts of our Service.</p>
+          </li>
+          <li>
+            <p><strong>Inactive</strong> means a organization account that has not engaged with or accessed the service within a 2-year period.</p>
+          </li>
+          <li>
+            <p><strong>Service</strong> refers to the Website.</p>
+          </li>
+          <li>
+            <p><strong>You</strong> means the individual accessing or using the Service, or the company, or other legal entity on behalf of which such individual is accessing or using the Service, as applicable.</p>
+          </li>
+        </ul>
+        <h2>What Happens When Your Account is Inactive?</h2>
+        <p>When you don't use your Wildlife Rehabilitation MD (WRMM) Account within a 2-year period, it's then deemed inactive, and all of its content and data can be deleted. Before this happens, you have the opportunity to take action in your account when We send you notifications to your Account email.</p>
+        <p>Prior to deleting your Account, We may mark your Account as suspended. In a suspended condition, the Account data will still exists however You and your users will not be able to authenticate into the Account.</p>
+        <h3>Recover a Suspended Account</h3>
+        <p>If your Account has become suspended, then you may request reactivation by contacting us by email at <a href="mailto:support@wildneighborsdp.org?subject=Reactivate%20suspended%20WRMD%20account">support@wildneighborsdp.org</a>. The request must come from the name and email address matching the Account contact information. If those details are no longer available, please mail us a letter on your organization letterhead to The Wild Neighbors Database Project, PO Box 421 Middletown, CA 95461. Additionally, we may make a reasonable assumption based on available evidence that reactivating your Account is warranted.</p>
         <h2>Changes to this Inactive Account Policy</h2>
         <p>We may update Our Inactive Account Policy from time to time. We will notify You of any changes by posting the new Inactive Account Policy on this page.</p>
         <p>We will let You know via email and/or a prominent notice on Our Service, prior to the change becoming effective and update the &quot;Last updated&quot; date at the top of this Inactive Account Policy.</p>
