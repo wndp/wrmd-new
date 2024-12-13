8a5fae3a8dd32c87568cf042cde2464c4b1e6530
Devin 2015 MacBook <dddesigns3@gmail.com>
2024-12-13 14:36:38 -0800

    Update WNDP text

--- resources/js/Pages/Public/Wndp.vue
+++ resources/js/Pages/Public/Wndp.vue
@@ -1,3 +1,7 @@
+<script setup>
+import PublicLayout from '@/Layouts/PublicLayout.vue';
+</script>
+
 <template>
   <PublicLayout title="About Wildlife Rehabilitation MD">
     <div class="relative py-8 px-4 sm:px-6 lg:px-8 bg-white">
@@ -17,17 +21,16 @@
           </div>
         </div>
         <p class="mt-8 text-xl text-gray-500 leading-8">
-          Wildlife Rehabilitation MD is developed by <a href="https://www.wildneighborsdp.org/" target="_blank">The Wild Neighbors Database Project</a>, a US 501(c)3 non-profit organization which promotes international wildlife rehabilitation by developing online data management software and helping implement its use worldwide to gather and exchange wildlife data.
+          Wildlife Rehabilitation MD (WRMD) is developed by <a
+            href="https://www.wildneighborsdp.org/"
+            target="_blank"
+          >The Wild Neighbors Database Project</a>, a US 501(c)3 non-profit organization which promotes international wildlife rehabilitation by developing online data management software and helping implement its use worldwide to gather and exchange wildlife data.
         </p>
       </div>
       <div class="mt-6 prose prose-xl prose-blue text-gray-500 mx-auto">
-        <p>Our founders, Rachel Avilla and Devin Dombrowski, have been wildlife rehabilitators since 2003. They saw a need for a wildlife database that was simple for everyone to use but scalable to large facilities. Devin is a self taught programmer and set out to create the Wildlife Rehablitation Medical Database, commonly referred to as WRMD. Development of WRMD began in 2010 and beta testing took place in 2012. WRMD was officially launched in 2013 and The Wild Neighbors Database Project was incorporated as a nonprofit in order to support WRMD and our other projects.</p>
-        <p>Flash forward to today and WRMD is used all over the world. As of 2023, WRMD is used in all 50 states, 33 countries, has over 1,400 accounts and over 3 million wild patients admitted. We continue to play an active role in WRMD by providing ongoing user support and continuing to improve WRMD behind the scenes with updated features and new versions.</p>
+        <p>Our founders, Rachel Avilla and Devin Dombrowski, have been wildlife rehabilitators since 2003. They saw a need for a wildlife database that was simple for everyone to use but scalable to large facilities. Devin was a self taught programmer and set out to create the Wildlife Rehablitation Medical Database, commonly referred to as WRMD. Development of WRMD began in 2010 and beta testing took place in 2012. WRMD was officially launched in 2013 and The Wild Neighbors Database Project was incorporated as a nonprofit in order to support WRMD and our other projects.</p>
+        <p>Flash forward to today and WRMD is used all over the world. As of 2025, WRMD is used in all 50 states, 51 countries, has over 1,775 accounts and over 4 million wild patients admitted. We continue to play an active role in WRMD by providing ongoing user support and continuing to improve WRMD behind the scenes with updated features and new versions.</p>
       </div>
     </div>
   </PublicLayout>
 </template>
-
-<script setup>
-import PublicLayout from '@/Layouts/PublicLayout.vue';
-</script>
