f64c4821abfef4e080c2014773ad75da63c39dfa
Devin 2015 MacBook <dddesigns3@gmail.com>
2024-12-13 10:52:47 -0800

    show helpscout beacon in staging and production

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -41,11 +41,11 @@
         <noscript>You must enable JavaScript to use Wildlife Rehabilitation MD (WRMD).</noscript>
         @inertia
     </body>
-    @production
+    @env(['staging', 'production'])
         <script type="text/javascript">!function(e,t,n){function a(){var e=t.getElementsByTagName("script")[0],n=t.createElement("script");n.type="text/javascript",n.async=!0,n.src="https://beacon-v2.helpscout.net",e.parentNode.insertBefore(n,e)}if(e.Beacon=n=function(t,n,a){e.Beacon.readyQueue.push({method:t,options:n,data:a})},n.readyQueue=[],"complete"===t.readyState)return a();e.attachEvent?e.attachEvent("onload",a):e.addEventListener("load",a,!1)}(window,document,window.Beacon||function(){});
         </script>
         <script type="text/javascript">
           window.Beacon('init', 'a9e2b564-4328-4fa2-871e-ab79a1dc43d8')
         </script>
-    @endproduction
+    @endenv
 </html>
