0ca2922d296c831a83817ce1ad77c9817e78f670
Devin 2015 MacBook <dddesigns3@gmail.com>
2024-12-14 10:01:09 -0800

    Fixed Donate Header

--- resources/js/Layouts/Partials/DonateHeader.vue
+++ resources/js/Layouts/Partials/DonateHeader.vue
@@ -1,6 +1,7 @@
 <script setup>
 import {computed} from 'vue';
 import {HeartIcon} from '@heroicons/vue/24/outline';
+import {__} from '@/Composables/Translate';
 
 const year = computed(() => new Date().getFullYear() + 1);
 </script>
@@ -23,17 +24,17 @@ const year = computed(() => new Date().getFullYear() + 1);
               {{ __('Fewer than 5% of our users have donated, but if everyone seeing this gave just $25 we would be set for :year.', {year}) }} <span class="font-bold whitespace-nowrap">{{ __('Can You Help?') }}</span>
             </span>
             <span class="hidden md:inline text-gray-500 mr-8">
-              {{ __("Wildlife Rehabilitation MD (WRMD) is a not-for-profit program serving over 1,350 wildlife rehabilitation organizations across 27 countries. It's free to use but not free for us to maintain.") }} <span class="bg-yellow-100 px-1">{{ __('Fewer than 5% of our users have donated, but if everyone seeing this gave just $25 we would be set for :year.', {year}) }}</span> <span class="font-bold whitespace-nowrap">{{ __('Can You Help?') }}</span>
+              {{ __("Wildlife Rehabilitation MD (WRMD) is a not-for-profit program serving over 1,775 wildlife rehabilitation organizations across 52 countries. It's free to use but not free for us to maintain.") }} <span class="bg-yellow-100 px-1">{{ __('Fewer than 5% of our users have donated, but if everyone seeing this gave just $25 we would be set for :year.', {year}) }}</span> <span class="font-bold whitespace-nowrap">{{ __('Can You Help?') }}</span>
             </span>
             <span class="block mt-1 sm:inline-block text-red-500">
               <Link
-                :href="route('donate.index', {'defaultInterval': 'm'})"
+                :href="route('donate.index')"
                 class="text-sm font-bold underline mr-8"
-              >{{ __('Give Monthly') }} <span aria-hidden="true">&rarr;</span></Link>
+              >{{ __('Give $25') }} <span aria-hidden="true">&rarr;</span></Link>
               <Link
-                :href="route('donate.index', {'defaultInterval': 'o'})"
+                :href="route('donate.index')"
                 class="text-sm font-bold underline"
-              >{{ __('Give Now') }} <span aria-hidden="true">&rarr;</span></Link>
+              >{{ __('Give a different amount') }} <span aria-hidden="true">&rarr;</span></Link>
             </span>
           </p>
         </div>
